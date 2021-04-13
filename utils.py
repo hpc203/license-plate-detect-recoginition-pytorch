f3662b87838413d6cb5cae215390ca00be838ecd
hpc203 <1749069040@qq.com>
2021-04-13 12:20:32 +0800

    Add files via upload

--- utils.py
+++ utils.py
@@ -94,25 +94,23 @@ def decode_landm(pre, priors, variances):
     return landms
 
 class PriorBox(object):
-    def __init__(self, cfg, image_size=None, phase='train'):
+    def __init__(self, cfg):
         super(PriorBox, self).__init__()
         self.min_sizes = cfg['min_sizes']
         self.steps = cfg['steps']
         self.clip = cfg['clip']
-        self.image_size = image_size
-        self.feature_maps = [[ceil(self.image_size[0]/step), ceil(self.image_size[1]/step)] for step in self.steps]
-        self.name = "s"
 
-    def forward(self):
+    def __call__(self, image_size):
+        feature_maps = [[ceil(image_size[0] / step), ceil(image_size[1] / step)] for step in self.steps]
         anchors = []
-        for k, f in enumerate(self.feature_maps):
+        for k, f in enumerate(feature_maps):
             min_sizes = self.min_sizes[k]
             for i, j in product(range(f[0]), range(f[1])):
                 for min_size in min_sizes:
-                    s_kx = min_size / self.image_size[1]
-                    s_ky = min_size / self.image_size[0]
-                    dense_cx = [x * self.steps[k] / self.image_size[1] for x in [j + 0.5]]
-                    dense_cy = [y * self.steps[k] / self.image_size[0] for y in [i + 0.5]]
+                    s_kx = min_size / image_size[1]
+                    s_ky = min_size / image_size[0]
+                    dense_cx = [x * self.steps[k] / image_size[1] for x in [j + 0.5]]
+                    dense_cy = [y * self.steps[k] / image_size[0] for y in [i + 0.5]]
                     for cy, cx in product(dense_cy, dense_cx):
                         anchors += [cx, cy, s_kx, s_ky]
                         #print(anchors)
